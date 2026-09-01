7096b15d17abd60b7bfa6a4d4ccf3d264663df9c
Inkt13 <138111203+Inkt13@users.noreply.github.com>
2026-09-01 19:36:55 +0800

    chore: add env

--- server/main.go
+++ server/main.go
@@ -1,16 +1,60 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		user,
+		password,
+		host,
+		port,
+		dbName,
+	)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected!")
+	_ = db
+	addUser(db, "Bob")
 }
 
-func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+func addUser(db *sql.DB, username string) {
+	result, err := db.Exec(
+		"INSERT INTO users (username) VALUES (?)",
+		username,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Created user:", id)
 }
