63b915233f82350729a796db71a706f60e0bfdca
mangathemango <163281872+mangathemango@users.noreply.github.com>
2026-09-04 16:55:46 +0700

    feat: add message sending functionality

---
+++ client/script.js
@@ -0,0 +1,54 @@
+const messageInput = document.getElementById("message-input")
+const convoContainer = document.getElementById("convo-container")
+
+
+messageInput.addEventListener("keydown", (event) => {
+    if (event.key == "Enter" && !event.ctrlKey) {
+        console.log("hmm")
+        event.preventDefault()
+        const content = messageInput.value.trim()
+        sendMessage("mango", "1:21", content)
+        messageInput.value = ""
+    }
+    if (event.key == "Enter" && event.ctrlKey) {
+        console.log("oh?")
+        messageInput.value += "\n"
+    }
+})
+
+const sendMessage = (user, time, content) => {
+    convoContainer.appendChild(messageBox(user, time, content))
+}
+
+const messageBox = (user, time, content) => {
+    const template = document.createElement("div")
+    template.innerHTML = `
+        <div class="message-box">
+            <img class="message-pfp" src="assets/image.png" alt="">
+            <div class="message-box-text">
+                <p class="message-box-user"><span class="message-box-username"></span> <span
+                        class="message-box-time"></span></p>
+                <p class="message-content"></p>
+            </div>
+        </div>
+    `
+    const element = template.querySelector(".message-box");
+    element.querySelector('.message-box-username').textContent = user
+    element.querySelector('.message-box-time').textContent = time
+    element.querySelector('.message-content').textContent = content
+
+    return element
+}
+
+const dateLine = (date) => {
+    const template = document.createElement('div')
+    template.innerHTML = `
+        <div class="date-line">
+            <div></div>
+            <p></p>
+            <div></div>
+        </div>
+    `
+    template.querySelector("p").textContent = date;
+    return template.querySelector("date-line")
+}
