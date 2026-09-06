596345f74d1aaeb782a2ceeed0f03943149421b3
Modi <narmagambetov05@gmail.com>
2026-09-06 17:43:12 +0800

    feat: user and conversation connection

--- server/main.go
+++ server/main.go
@@ -45,7 +45,7 @@ func main() {
 		log.Fatalf("Database connection failed: %v", err)
 	}
 
-	http.HandleFunc("/users", handleUsers)
+	http.HandleFunc("/login", LoginHandler)
 	http.HandleFunc("/newUser", NewUser)
 	http.HandleFunc("/me", handleMe)
 	http.HandleFunc("/logout", LogoutHandler)
@@ -69,22 +69,22 @@ func handleConversations(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		getConversation(w, r)
 	case http.MethodPost:
-		postConversation(w, r)
+		CreateConversation(w, r)
 	}
 }
 
-func handleUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+// func handleUsers(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
 
-	switch r.Method {
-	case http.MethodGet:
-		getUsers(w, r)
-	case http.MethodPost:
-		LoginHandler(w, r)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
+// 	switch r.Method {
+// 	case http.MethodGet:
+// 		getUsers(w, r)
+// 	case http.MethodPost:
+// 		LoginHandler(w, r)
+// 	default:
+// 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// 	}
+// }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT username FROM users")
@@ -189,7 +189,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		SameSite: http.SameSiteLaxMode,
 	})
 
-	query = "INSERT INTO session (username, cookie, createdAt) values (?, ?, ?)"
+	query = "INSERT INTO sessions (username, session_token, createdAt) values (?, ?, ?)"
 
 	_, err = db.Exec(query, UserIn, sessionID, time.Now())
 
@@ -211,9 +211,7 @@ func handleMe(w http.ResponseWriter, r *http.Request) {
 
 	var username string
 
-	query := "SELECT username FROM session WHERE cookie = ?"
-
-	err = db.QueryRow(query, cookie.Value).Scan(&username)
+	err = db.QueryRow("SELECT username FROM sessions WHERE session_token = ?", cookie.Value).Scan(&username)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -247,7 +245,7 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	query := "DELETE FROM session where cookie = ?"
+	query := "DELETE FROM sessions where cookie = ?"
 
 	_, err = db.Exec(query, cookie.Value)
 
@@ -284,7 +282,7 @@ func delUser(w http.ResponseWriter, r *http.Request) {
 
 	var usern string
 
-	err = db.QueryRow("SELECT username FROM session WHERE cookie = ?", cookie.Value).Scan(&usern)
+	err = db.QueryRow("SELECT username FROM sessions WHERE session_token = ?", cookie.Value).Scan(&usern)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -300,7 +298,7 @@ func delUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = db.Exec("DELETE FROM session where cookie = ?", cookie.Value)
+	_, err = db.Exec("DELETE FROM sessions where session_token = ?", cookie.Value)
 
 	if err != nil {
 		http.Error(w, "Cookie not found", http.StatusInternalServerError)
@@ -325,16 +323,26 @@ func delUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func getConversation(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
 }
 
-func postConversation(w http.ResponseWriter, r *http.Request) {
+func CreateConversation(w http.ResponseWriter, r *http.Request) {
 
 	var conv Conversation
 
-	// cookie, err := r.Cookie("session_token")
+	cookie, err := r.Cookie("session_token")
 
-	err := json.NewDecoder(r.Body).Decode(&conv)
+	if err != nil {
+		if err == http.ErrNoCookie {
+			http.Error(w, "Unauthorised", http.StatusUnauthorized)
+			return
+		}
+		http.Error(w, "Bitch no cookie to you", http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&conv)
 	if err != nil {
 		http.Error(w, "The new convo json was not decoded", http.StatusInternalServerError)
 		return
@@ -342,12 +350,38 @@ func postConversation(w http.ResponseWriter, r *http.Request) {
 
 	// err = models.CreateConvo(conv.Name)
 
-	query := "INSERT INTO conversations (name) VALUES (?)"
-
-	_, err = db.Exec(query, conv.Name)
+	result, err := db.Exec("INSERT INTO conversations (name) VALUES (?)", conv.Name)
 
 	if err != nil {
-		http.Error(w, "Something happened in the createConvo in db", http.StatusInternalServerError)
+		http.Error(w, "Something happened while inserting into 'conversations' table in db", http.StatusInternalServerError)
+		return
+	}
+
+	conversationID, err := result.LastInsertId()
+
+	if err != nil {
+		http.Error(w, "Problem occured when getting the id of the convo", http.StatusInternalServerError)
+		return
+	}
+
+	var userId int
+
+	err = db.QueryRow(`SELECT users.id FROM sessions JOIN users ON sessions.username = users.username WHERE sessions.session_token = ?`, cookie.Value).Scan(&userId)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "User doesnt exist", http.StatusInternalServerError)
+			return
+		}
+
+		http.Error(w, "Problem while scanning for user ID occured", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = db.Exec("INSERT into user_in_conversation (user_id, conversation_id) VALUES (?, ?)", userId, conversationID)
+
+	if err != nil {
+		http.Error(w, "something happend while inserting data into 'user_in_conversation' table", http.StatusInternalServerError)
 		return
 	}
 
@@ -355,5 +389,13 @@ func postConversation(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{
 		"message": "The new convo was created",
 	})
+
+}
+
+func JoinConvo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UserInConversation(w http.ResponseWriter, r *http.Request) {
 
 }
