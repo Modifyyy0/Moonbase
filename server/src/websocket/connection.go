4a3a1efd87b9e67be3718496edd048a8c5988c84
Inkt13 <138111203+Inkt13@users.noreply.github.com>
2026-09-11 21:43:15 +0800

    refactor: fixing connection.go with correct sessionTokens

--- server/src/websocket/connection.go
+++ server/src/websocket/connection.go
@@ -4,19 +4,28 @@ import (
     "log"
     "net/http"
     "github.com/gorilla/websocket"
+	"Moonbase/src/models"
 )
 
 var upgrader = websocket.Upgrader{}
 
 func CreateConnection(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 
-	conn, err := upgrader.Upgrade(w, r, nil)
-
+	cookie, err := r.Cookie("session_token")
 	if err != nil {
 		return nil, err
 	}
 
-	log.Println("WebSocket connection established")
+	_, err = models.FindSessionByToken(cookie.Value)
+	if err != nil {
+        return nil, err
+    }
 
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+        return nil, err
+    }
+	
+	log.Println("WebSocket connection established")
 	return conn, nil
 }
