2bb4d4f8245e220648f2434f8770858ff759b298
Modi <narmagambetov05@gmail.com>
2026-09-11 17:45:48 +0800

    fix: /api endpoint

--- server/main.go
+++ server/main.go
@@ -21,18 +21,18 @@ func main() {
 	}
 	defer db.Close()
 
-	http.HandleFunc("POST /login", handlers.LoginHandler)
-	http.HandleFunc("POST /logout", handlers.LogoutHandler)
-	http.HandleFunc("GET /me", handlers.HandleMe)
+	http.HandleFunc("POST /api/login", handlers.LoginHandler)
+	http.HandleFunc("POST /api/logout", handlers.LogoutHandler)
+	http.HandleFunc("GET /api/me", handlers.HandleMe)
 
-	http.HandleFunc("/conversations", handlers.HandleConversations)
-	http.HandleFunc("GET /conversations/{convoID}", handlers.ConvoInfo)
-	http.HandleFunc("DELETE /conversations/{convoID}/members/me", handlers.LeaveConvo)
-	http.HandleFunc("GET /conversations/{convoID}/messages", handlers.GetMessages)
-	http.HandleFunc("POST /join/{convoID}", handlers.JoinConvo)
+	http.HandleFunc("/api/conversations", handlers.HandleConversations)
+	http.HandleFunc("GET /api/conversations/{convoID}", handlers.ConvoInfo)
+	http.HandleFunc("DELETE /api/conversations/{convoID}/members/me", handlers.LeaveConvo)
+	http.HandleFunc("GET /api/conversations/{convoID}/messages", handlers.GetMessages)
+	http.HandleFunc("POST /api/join/{convoID}", handlers.JoinConvo)
 
-	http.HandleFunc("POST /newUser", handlers.NewUser)
-	http.HandleFunc("DELETE /deleteUser", handlers.DeleteUser)
+	http.HandleFunc("POST /api/newUser", handlers.NewUser)
+	http.HandleFunc("DELETE /api/deleteUser", handlers.DeleteUser)
 
 	fmt.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
